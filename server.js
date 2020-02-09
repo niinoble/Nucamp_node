97169b32ea730bc60a168b2bdb4e593febf80ea5
Noble Amarteifio <gi1275@wayne.edu>
2020-02-09 07:38:54 -0500

    Assignment 1 and start of week 2

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const campsiteRouter = require('./routes/campsiteRouter');
-
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = '3000';
@@ -11,6 +12,8 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 
 
